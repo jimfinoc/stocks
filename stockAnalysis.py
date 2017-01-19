f9fed725620f7148b7e4c9878a86e5f2e20c5529
J <jimfinoc@gmail.com>
2017-01-19 18:10:21 -0500

    All but dates are manipulated and saved. Dates are saved but need to be standardized in the parsing.

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -14,10 +14,11 @@ else: #create the File
     c = conn.cursor()
     c.execute('''
     CREATE TABLE transactions (
-    id INTEGER PRIMARY KEY,
-    transactionType TEXT,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    action TEXT,
+    type TEXT,
     stock TEXT,
-    transactionDate TEXT,
+    date TEXT,
     comission REAL,
     fees REAL,
     price REAL,
@@ -25,3 +26,28 @@ else: #create the File
     ''')
     conn.commit()
     conn.close()
+
+
+if os.path.isfile(sqlite_file):
+    sqlite_file = 'my_stocks.sqlite'    # name of the sqlite database file
+    # Connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+    print "Please enter the data for the stock purchase."
+    dateInput = raw_input("Date in format of (2016-12-31):").upper()
+    actionInput = raw_input('Action (BUY or SELL):').upper()
+    stockInput = raw_input("Stock symbol (F):").upper()
+    typeInput = raw_input("Commodity (STOCK):").upper()
+    priceInput = float(raw_input("Transaction Price (10.00):"))
+    comissionInput = float(raw_input("Comission costs (8.00):"))
+    feesInput = float(raw_input("Fees (1.00):"))
+    quantityInput = int(raw_input("Quantity traded (100):"))
+    # Do this instead
+    print "Your transaction is as follows:"
+    statement = (actionInput, typeInput, stockInput, dateInput, comissionInput, feesInput, priceInput, quantityInput)
+    print statement
+    c.execute('INSERT INTO transactions VALUES (NULL,?,?,?,?,?,?,?,?)', statement)
+    conn.commit()
+    conn.close()
+else: #create the File
+    print "Something is wrong with the file."
