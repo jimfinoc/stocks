9e5e61dd835114b6c90a8be3a235c5efdacecdb1
J <jimfinoc@gmail.com>
2017-01-20 21:10:07 -0500

    importing now works!

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 from yahoo_finance import Share
 import os.path
@@ -5,8 +6,11 @@ import datetime
 
 # check to see if the database file exist and if not, create it.
 sqlite_file = 'my_stocks.sqlite'
+import_file = "import_stock.csv"
+
 if os.path.isfile(sqlite_file):
-    print "File looks like it exist."
+    # print "File looks like it exist."
+    pass
 else: #create the File
     print "Creating the database."
     sqlite_file = 'my_stocks.sqlite'    # name of the sqlite database file
@@ -39,7 +43,15 @@ def refresh_positions():
     #   lets look up price per share
     #   data = Share(stockSymbol)
     #   data.get_price()
-    pass
+    if os.path.isfile(sqlite_file):
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        c.execute('SELECT DISTINCT stock FROM transactions')
+        print c.fetchall()
+        conn.commit()
+        conn.close()
+    else: #create the File
+        pass
 
 def add_transaction():
     if os.path.isfile(sqlite_file):
@@ -69,7 +81,11 @@ def add_transaction():
                                 dateValue = datetime.datetime.strptime(dateInput,'%m/%d/%y')
                                 break
                             except:
-                                print "I don't understand that date, please try again."
+                                try:
+                                    dateValue = datetime.datetime.strptime(dateInput,"%d-%b-%Y")
+                                    break
+                                except:
+                                    print "I don't understand that date, please try again."
         print "I think your date is: ", str(dateValue.date())
         actionInput = ""
         while actionInput == "":
@@ -104,9 +120,9 @@ def add_transaction():
             except:
                 print "I'm going to need you to try that again"
         if actionInput == "BUY":
-            total = quantityInput * priceInput + comissionInput + feesInput
+            total = round(quantityInput * priceInput + comissionInput + feesInput,2)
         elif actionInput == "SELL":
-            total = quantityInput * priceInput - comissionInput - feesInput
+            total = round(quantityInput * priceInput - comissionInput - feesInput,2)
         print "Your transaction is as follows:"
         statement = (actionInput, typeInput, stockInput, str(dateValue.date()), comissionInput, feesInput, priceInput, quantityInput, total)
         print statement
@@ -116,24 +132,77 @@ def add_transaction():
     else: #create the File
         print "Something is wrong with the file."
 
+def import_transactions():
+    if os.path.isfile(sqlite_file) and os.path.isfile(import_file):
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        print "Please ensure your data is in a comma seperated value format of"
+        print "Action, Type, Symbol, Date, Price, Quantity, Comission, & Fees"
+        print "or as an example:"
+        print "BUY, STOCK, F, 2016-12-31, 10.00, 1000, 8.95, 0"
+        print "the file should be named import_stock.csv"
+        print ""
+        print "the statement will be recorded as follows:"
+        print "(actionInput, typeInput, stockInput, date, comissionInput, feesInput, priceInput, quantityInput, total)"
+        with open(import_file, 'rb') as f:
+            data = csv.reader(f)
+            for row in data:
+                # print row
+                actionInput = row[0].upper()
+                typeInput = row[1].upper().replace(" ","",1)
+                stockInput = row[2].upper().replace(" ","",1)
+                try:
+                    dateValue = datetime.datetime.strptime(row[3].replace(" ","",1),"%Y-%m-%d")
+                except:
+                    dateValue = datetime.datetime.strptime(row[3].replace(" ","",1),"%d-%b-%y")
+                priceInput = float(row[4].replace(" ","",1))
+                quantityInput = int(row[5].replace(" ","",1))
+                comissionInput = float(row[6].replace(" ","",1))
+                feesInput = float(row[7].replace(" ","",1))
+                if actionInput == "BUY":
+                    total = round(quantityInput * priceInput + comissionInput + feesInput,2)
+                elif actionInput == "SELL":
+                    total = round(quantityInput * priceInput - comissionInput - feesInput,2)
+                print "recorded: ",
+                statement = (actionInput, typeInput, stockInput, str(dateValue.date()), comissionInput, feesInput, priceInput, quantityInput, total)
+                print statement
+                c.execute('INSERT INTO transactions VALUES (NULL,?,?,?,?,?,?,?,?,?)', statement)
+        conn.commit()
+        conn.close()
+
+def view_transactions():
+    if os.path.isfile(sqlite_file):
+        # Connecting to the database file
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+        c.execute('SELECT * FROM transactions')
+        print c.fetchall()
+        conn.commit()
+        conn.close()
+    else: #create the File
+        print "Something is wrong with the file."
+
+
+
+print "You're in the stock program. Time to make some money!"
 while True:
-    print "You're in the stock program. Time to make some money!"
     print ""
     print "Your current positions are as follow:"
     refresh_positions()
     # print "THE POSITIONS"
-    print ("""
-    1. View transaction list
-    2. Add a transaction
-    3. Quit
-    """)
+    print "1. View transaction list"
+    print "2. Add a transaction"
+    print "3. Import transactions from ", import_file
+    print "Q. Quit"
     ans=raw_input("What would you like to do? ")
     if ans=="1":
-        pass
+        view_transactions()
     elif ans=="2":
         add_transaction()
     elif ans=="3":
-        print("\n Goodbye")
+        import_transactions()
+    elif ans=="q" or ans=="Q":
+        print("Goodbye")
         break
     elif ans !="":
         print("\n Not Valid Choice Try again")
